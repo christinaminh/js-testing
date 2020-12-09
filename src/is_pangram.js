35baa3f4caeffce2c5c522416d2494286f8667a5
christinaminh <christina.minh@gmail.com>
2020-12-09 15:46:55 -0800

    Completed

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,25 @@
-const isPangram = function(text) {
+const { TestScheduler } = require("jest");
 
+const isPangram = function(text) {
+  const alphabet = 'abcdefghijklkmnopqrstuvwxyz'
+
+  const lettersUsed = {};
+
+  for (const letter of text) {
+    if (!lettersUsed[letter]) {
+      lettersUsed[letter] = true;
+    }
+  }
+
+  for (const letter of alphabet) {
+    if (!lettersUsed[letter]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 module.exports = isPangram;
+
+isPangram('text');
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -10,54 +10,76 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toBe(true)
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = 'abcdefghijklmnopqrstuvwxyz';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(true)
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const text = 'abcdefghijklmnopqrstuvwyz';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(false)
   });
 
   test('empty sentence', () => {
     // Arrange
+    const text = '';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(false)
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(true)
   });
 
   test('pangram with numbers', () => {
     // Arrange
+    const text = 'the quick brown fox jumps over the lazy dog 100 times';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
-
+    expect(answer).toBe(true)
   });
 
   // Write your own test case
+  test('pangram with non-alphanumeric characters', () => {
+    // Arrange
+    const text = 'the quick brown fox jumps over the lazy dog!!!!!';
+
+    // Act
+    const answer = isPangram(text);
+
+    // Assert
+    expect(answer).toBe(true)
+  });
 });
